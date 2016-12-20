a27f2c81346ba1c43458df7528e0fb2c3eea9f5f
Guilherme Fiola <guilhermefiola@gmail.com>
2016-12-20 00:40:23 -0200

    Aula 02

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,13 @@ var numPalavras = frase.split(" ").length;
 
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
+
+campo = $(".campo-digitacao");
+campo.on("input", function(){
+    var conteudo = campo.val();
+    var qtdePalavras = conteudo.split(/\S+/).length - 1;
+    $(".contador-palavras").text(qtdePalavras);
+
+    var contadorCaracteres = conteudo.length;
+    $(".contador-caracteres").text(contadorCaracteres); 
+});
