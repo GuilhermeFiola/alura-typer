3d6bc743388ad89709b75f848455c11dc865217f
GuilhermeFiola <guilhermefiola@gmail.com>
2016-12-24 15:29:07 -0200

    Aula 04

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,54 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
+var tempoInicial = $("#tempo-digitacao").text();
+var campo = $("#campo-digitacao");
 
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
-campo = $(".campo-digitacao");
-campo.on("input", function(){
-    var conteudo = campo.val();
-    var qtdePalavras = conteudo.split(/\S+/).length - 1;
-    $(".contador-palavras").text(qtdePalavras);
-
-    var contadorCaracteres = conteudo.length;
-    $(".contador-caracteres").text(contadorCaracteres); 
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function(){
-    var cronometroId = setInterval(function(){
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if(tempoRestante<1){  
-            campo.attr("disabled", true);
-            clearInterval(cronometroId);
-        };
-    }, 1000);
-});
+function atualizaTamanhoFrase(){
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+};
+
+
+function inicializaContadores(){
+    campo.on("input", function(){
+        var conteudo = campo.val();
+        var qtdePalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdePalavras);
+
+        var contadorCaracteres = conteudo.length;
+        $("#contador-caracteres").text(contadorCaracteres); 
+    });
+};
+
+function inicializaCronometro(){
+    var botaoReiniciar = $("#botao-reiniciar");
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus", function(){
+        var cronometroId = setInterval(function(){
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if(tempoRestante<1){  
+                campo.attr("disabled", true);
+                botaoReiniciar.attr("disabled", false);
+                clearInterval(cronometroId);
+            };
+        }, 1000);
+        botaoReiniciar.attr("disabled", true);
+    });
+};
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+};
